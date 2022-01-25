29512091180b368b865d7a57d32da75dcf5d1bdd
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-01-26 04:07:25 +0700

    Membuat halaman detail message

---
+++ lib/screens/messages/message_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageDetail extends StatefulWidget {
+  const MessageDetail({Key? key}) : super(key: key);
+
+  @override
+  State<MessageDetail> createState() => _MessageDetailState();
+}
+
+class _MessageDetailState extends State<MessageDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

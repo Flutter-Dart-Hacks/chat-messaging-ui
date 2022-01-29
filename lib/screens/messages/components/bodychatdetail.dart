a0def1500a469dfae2b5de635974f46f10059cac
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-01-30 04:21:25 +0700

    Perbaikan ripple effect di message detail

--- lib/screens/messages/components/bodychatdetail.dart
+++ lib/screens/messages/components/bodychatdetail.dart
@@ -34,7 +34,9 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
           child: SafeArea(
             child: Row(
               children: [
-                InkWell(
+                Material(
+                  // Menggunakan parent material
+                  // https://stackoverflow.com/a/65760259
                   child: IconButton(
                     onPressed: () {},
                     icon: const Icon(
@@ -42,8 +44,11 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
                       color: kPrimaryColor,
                     ),
                     padding: const EdgeInsets.only(left: 0, right: 0),
+                    splashColor: Colors.amber,
                   ),
-                  onTap: () {},
+                  color: Colors.transparent,
+                  shape: const CircleBorder(),
+                  clipBehavior: Clip.hardEdge,
                 ),
                 const SizedBox(
                   width: 1,
@@ -57,16 +62,22 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
                     ),
                     child: Row(
                       children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.sentiment_satisfied_alt_outlined,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1
-                                ?.color
-                                ?.withOpacity(0.64),
+                        Material(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.sentiment_satisfied_alt_outlined,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.color
+                                  ?.withOpacity(0.64),
+                            ),
+                            splashColor: Colors.amber,
                           ),
+                          color: Colors.transparent,
+                          shape: const CircleBorder(),
+                          clipBehavior: Clip.hardEdge,
                         ),
                         Expanded(
                           child: TextField(
@@ -79,29 +90,41 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
                             style: const TextStyle(fontSize: 14),
                           ),
                         ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.attach_file,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1
-                                ?.color
-                                ?.withOpacity(0.64),
+                        Material(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.attach_file,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.color
+                                  ?.withOpacity(0.64),
+                            ),
+                            splashColor: Colors.amber,
+                            padding: const EdgeInsets.only(left: 0, right: 0),
                           ),
-                          padding: const EdgeInsets.only(left: 0, right: 0),
+                          color: Colors.transparent,
+                          shape: const CircleBorder(),
+                          clipBehavior: Clip.hardEdge,
                         ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.camera_alt_outlined,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1
-                                ?.color
-                                ?.withOpacity(0.64),
+                        Material(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.color
+                                  ?.withOpacity(0.64),
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 2),
+                            splashColor: Colors.amber,
                           ),
-                          padding: const EdgeInsets.symmetric(horizontal: 2),
+                          color: Colors.transparent,
+                          shape: const CircleBorder(),
+                          clipBehavior: Clip.hardEdge,
                         ),
                       ],
                     ),

c6053157cbc4c41ff6b1aef5496a95d7c001aaf2
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-01-31 03:59:11 +0700

    Update input field component

--- lib/screens/messages/components/bodychatdetail.dart
+++ lib/screens/messages/components/bodychatdetail.dart
@@ -1,4 +1,4 @@
-import 'package:chatmessagings/constants.dart';
+import 'package:chatmessagings/screens/messages/components/chat_input_field.dart';
 import 'package:flutter/material.dart';
 
 class BodyMessageDetail extends StatefulWidget {
@@ -23,117 +23,8 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
     return Column(
       children: [
         const Spacer(),
-        Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: kDefaultPadding,
-            vertical: kDefaultPadding / 2,
-          ),
-          decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
-          ),
-          child: SafeArea(
-            child: Row(
-              children: [
-                Material(
-                  // Menggunakan parent material
-                  // https://stackoverflow.com/a/65760259
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.mic,
-                      color: kPrimaryColor,
-                    ),
-                    padding: const EdgeInsets.only(left: 0, right: 0),
-                    splashColor: Colors.amber,
-                  ),
-                  color: Colors.transparent,
-                  shape: const CircleBorder(),
-                  clipBehavior: Clip.hardEdge,
-                ),
-                const SizedBox(
-                  width: 1,
-                ),
-                Expanded(
-                  child: Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: kPrimaryColor.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    child: Row(
-                      children: [
-                        Material(
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.sentiment_satisfied_alt_outlined,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  ?.color
-                                  ?.withOpacity(0.64),
-                            ),
-                            splashColor: Colors.amber,
-                          ),
-                          color: Colors.transparent,
-                          shape: const CircleBorder(),
-                          clipBehavior: Clip.hardEdge,
-                        ),
-                        Expanded(
-                          child: TextField(
-                            decoration: const InputDecoration(
-                              hintText: 'Type message here',
-                              border: InputBorder.none,
-                            ),
-                            onChanged: (String value) {},
-                            controller: _chatContentEditingController,
-                            style: const TextStyle(fontSize: 14),
-                          ),
-                        ),
-                        Material(
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.attach_file,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  ?.color
-                                  ?.withOpacity(0.64),
-                            ),
-                            splashColor: Colors.amber,
-                            padding: const EdgeInsets.only(left: 0, right: 0),
-                          ),
-                          color: Colors.transparent,
-                          shape: const CircleBorder(),
-                          clipBehavior: Clip.hardEdge,
-                        ),
-                        Material(
-                          child: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.camera_alt_outlined,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  ?.color
-                                  ?.withOpacity(0.64),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 2),
-                            splashColor: Colors.amber,
-                          ),
-                          color: Colors.transparent,
-                          shape: const CircleBorder(),
-                          clipBehavior: Clip.hardEdge,
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        )
+        ChatInputField(
+            chatContentEditingController: _chatContentEditingController)
       ],
     );
   }
---
+++ lib/screens/messages/components/chat_input_field.dart
@@ -0,0 +1,144 @@
+import 'package:chatmessagings/constants.dart';
+import 'package:flutter/material.dart';
+
+class ChatInputField extends StatefulWidget {
+  const ChatInputField({
+    Key? key,
+    required TextEditingController chatContentEditingController,
+  })  : _chatContentEditingController = chatContentEditingController,
+        super(key: key);
+
+  final TextEditingController _chatContentEditingController;
+
+  @override
+  State<ChatInputField> createState() => _ChatInputFieldState();
+}
+
+class _ChatInputFieldState extends State<ChatInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: kDefaultPadding / 3,
+        right: kDefaultPadding,
+        top: kDefaultPadding / 2,
+        bottom: kDefaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 4),
+            blurRadius: 32,
+            color: const Color(0xff087949).withOpacity(0.1),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Material(
+              // Menggunakan parent material dan inkwell
+              // https://stackoverflow.com/a/65760259
+              child: InkWell(
+                onTap: () {},
+                child: const Padding(
+                  padding:
+                      EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+                  child: Icon(
+                    Icons.mic,
+                    color: kPrimaryColor,
+                  ),
+                ),
+                splashColor: Colors.amber,
+              ),
+              color: Colors.transparent,
+              shape: const CircleBorder(),
+              clipBehavior: Clip.hardEdge,
+            ),
+            const SizedBox(
+              width: 1,
+            ),
+            Expanded(
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Row(
+                  children: [
+                    Material(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.sentiment_satisfied_alt_outlined,
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.color
+                              ?.withOpacity(0.64),
+                        ),
+                        splashColor: Colors.amber,
+                      ),
+                      color: Colors.transparent,
+                      shape: const CircleBorder(),
+                      clipBehavior: Clip.hardEdge,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          hintText: 'Type message here',
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (String value) {},
+                        controller: widget._chatContentEditingController,
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ),
+                    Material(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.attach_file,
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.color
+                              ?.withOpacity(0.64),
+                        ),
+                        splashColor: Colors.amber,
+                        padding: const EdgeInsets.only(left: 0, right: 0),
+                      ),
+                      color: Colors.transparent,
+                      shape: const CircleBorder(),
+                      clipBehavior: Clip.hardEdge,
+                    ),
+                    Material(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.camera_alt_outlined,
+                          color: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.color
+                              ?.withOpacity(0.64),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 2),
+                        splashColor: Colors.amber,
+                      ),
+                      color: Colors.transparent,
+                      shape: const CircleBorder(),
+                      clipBehavior: Clip.hardEdge,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

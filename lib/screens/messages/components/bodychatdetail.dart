1af03333c1b72bb945fdc37ac077a38a844f9ada
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-02-01 15:27:18 +0700

    Proses pembuatan list message dan urutan pesannya

--- lib/screens/messages/components/bodychatdetail.dart
+++ lib/screens/messages/components/bodychatdetail.dart
@@ -1,4 +1,6 @@
+import 'package:chatmessagings/constants.dart';
 import 'package:chatmessagings/screens/messages/components/chat_input_field.dart';
+import 'package:chatmessagings/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class BodyMessageDetail extends StatefulWidget {
@@ -22,9 +24,56 @@ class _BodyMessageDetailState extends State<BodyMessageDetail> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Spacer(),
+        Expanded(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return MessageItem(
+                  chatMessage: demoChatMessages[index],
+                );
+              },
+              itemCount: demoChatMessages.length,
+            ),
+          ),
+        ),
         ChatInputField(
             chatContentEditingController: _chatContentEditingController)
+      ],
+    );
+  }
+}
+
+class MessageItem extends StatelessWidget {
+  const MessageItem({Key? key, required this.chatMessage}) : super(key: key);
+
+  final ChatMessage chatMessage;
+
+  MainAxisAlignment getAxisAlignmentContent(ChatMessage message) {
+    return message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: getAxisAlignmentContent(chatMessage),
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: kDefaultPadding),
+          padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding * 0.75,
+            vertical: kDefaultPadding / 2,
+          ),
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Text(
+            chatMessage.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
       ],
     );
   }
